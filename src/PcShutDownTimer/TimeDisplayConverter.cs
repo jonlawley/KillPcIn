50cd184297ca55410a4126e3fd998cdd6c646031
jlawley <jon@jonlawley.com>
2016-03-20 10:42:33 +1100

    1. Defect. Tool could not show hours when it was set to be run for over an hour. Switched for a converter that will show total hours when tool is run for over an hour

---
+++ src/PcShutDownTimer/TimeDisplayConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PcShutdownTimer
+{
+    public class TimeDisplayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var timeSpan = value as TimeSpan? ?? new TimeSpan();
+            if (timeSpan != TimeSpan.Zero)
+            {
+                return timeSpan.TotalHours > 1 ? $"{Math.Floor(timeSpan.TotalHours)}{timeSpan.ToString(@"\:mm\:ss")}":$"{timeSpan.ToString(@"mm\:ss\:ff")}";
+            }
+            return "00:00:00";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
